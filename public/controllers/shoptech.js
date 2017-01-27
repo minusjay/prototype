058084a0dd9e6e9602ba87e54e6566d7daa4aacd
Davisson, Adrienne <age.davisson@gmail.com>
2017-01-26 22:09:41 -0500

    Added truck table, map image, job details

--- public/controllers/shoptech.js
+++ public/controllers/shoptech.js
@@ -17,25 +17,19 @@ function ShopTechCtrl($scope) {
 	$scope.jobs = [
 		{
 			id:1001,
-			company:'Company Name 1',
-			type:'Lorem ipsum dolor sit amet'
+			company:'Bobs Auto',
+			type:'New Install'
 		},
 		{
 			id:1004,
-			company:'Company Name 3',
-			type:'Lorem ipsum dolor sit amet'
+			company:'Acme Corporation',
+			type:'Repair'
 		},
 		{
 			id:1023,
-			company:'Company Name234',
-			type:'Lorem ipsum dolor sit amet'
-		},
-		{
-			id:1121,
-			company:'Company Name541',
-			type:'Lorem ipsum dolor sit amet'
+			company:'Globex',
+			type:'Maintenance'
 		}
-
 	];
 }
 
@@ -46,86 +40,65 @@ function TechJobCtrl($scope,$routeParams,$filter) {
 	$scope.jobs = [
 		{
 			id:1001,
-			company:'Company Name 1',
-			type:'Lorem ipsum dolor sit amet',
+			company:'Bobs Auto',
+			type:'New Install',
 			startTime:'9:00 AM',
 			contact:{
-				name:'Steve Doe',
-				address:'1355 Market Street, Suit 900',
-				city:'Some City',
+				name:'Bob Other',
+				address:'1335 Market St',
+				city:'New City',
 				state:'NJ',
-				zip:'08000',
-				phone:'(123) 456-7890'
+				zip:'08245',
+				phone:'609-987-2345'
 			},
 			description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam esse voluptatum, reiciendis eius pariatur blanditiis vel ducimus doloribus. Sed id, fuga dolorem. Assumenda adipisci laboriosam rem deserunt fuga, eos at. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti cum accusantium magni expedita in ratione, ipsam rerum porro delectus quo. Eaque, voluptates repellat reiciendis ut ipsam odit molestiae expedita eius!',
 			instrucitons:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam esse voluptatum, reiciendis eius pariatur blanditiis vel ducimus doloribus. Sed id, fuga dolorem. Assumenda adipisci laboriosam rem deserunt fuga, eos at. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti cum accusantium magni expedita in ratione, ipsam rerum porro delectus quo. Eaque, voluptates repellat reiciendis ut ipsam odit molestiae expedita eius!',
 			parts:[
-				{id:203, name:'Cras justo odio',qty:14},
-				{id:203, name:'Rusto odio',qty:1},
-				{id:203, name:'Dio',qty:3}
+				{id:203, name:'Valve',qty:6},
+				{id:203, name:'Washer 3/4',qty:12},
+				{id:203, name:'Screw Cap 3/4',qty:5}
 			]
 		},
 		{
 			id:1004,
-			company:'Company Name 3',
-			type:'Lorem ipsum dolor sit amet',
+			company:'Acme Corporation',
+			type:'Repair',
 			startTime:'11:00 AM',
 			contact:{
-				name:'Steve Doe',
-				address:'1355 Market Street, Suit 900',
-				city:'Some City',
+				name:'Laverne S Smith',
+				address:'302 Rt 11',
+				city:'Galena',
 				state:'NJ',
-				zip:'08000',
-				phone:'(123) 456-7890'
+				zip:'08245',
+				phone:'417-357-1338'
 			},
 			description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam esse voluptatum, reiciendis eius pariatur blanditiis vel ducimus doloribus. Sed id, fuga dolorem. Assumenda adipisci laboriosam rem deserunt fuga, eos at. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti cum accusantium magni expedita in ratione, ipsam rerum porro delectus quo. Eaque, voluptates repellat reiciendis ut ipsam odit molestiae expedita eius!',
 			instrucitons:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam esse voluptatum, reiciendis eius pariatur blanditiis vel ducimus doloribus. Sed id, fuga dolorem. Assumenda adipisci laboriosam rem deserunt fuga, eos at. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti cum accusantium magni expedita in ratione, ipsam rerum porro delectus quo. Eaque, voluptates repellat reiciendis ut ipsam odit molestiae expedita eius!',
 			parts:[
-				{id:203, name:'Cras justo odio',qty:14},
-				{id:203, name:'Rusto odio',qty:1},
-				{id:203, name:'Dio',qty:3}
+				{id:203, name:'Nut HEX 3/4-10',qty:14},
+				{id:203, name:'Adapter 5FNP',qty:7},
+				{id:203, name:'CLPC HSE',qty:3}
 			]
 		},
 		{
 			id:1023,
-			company:'Company Name234',
-			type:'Lorem ipsum dolor sit amet',
+			company:'Globex',
+			type:'Maintenance',
 			startTime:'1:00 PM',
 			contact:{
-				name:'Steve Doe',
-				address:'1355 Market Street, Suit 900',
-				city:'Some City',
+				name:'Cora Smith',
+				address:'392 Frank St',
+				city:'Sharon',
 				state:'NJ',
-				zip:'08000',
-				phone:'(123) 456-7890'
+				zip:'08245',
+				phone:'609-538-7280'
 			},
 			description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam esse voluptatum, reiciendis eius pariatur blanditiis vel ducimus doloribus. Sed id, fuga dolorem. Assumenda adipisci laboriosam rem deserunt fuga, eos at. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti cum accusantium magni expedita in ratione, ipsam rerum porro delectus quo. Eaque, voluptates repellat reiciendis ut ipsam odit molestiae expedita eius!',
 			instrucitons:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam esse voluptatum, reiciendis eius pariatur blanditiis vel ducimus doloribus. Sed id, fuga dolorem. Assumenda adipisci laboriosam rem deserunt fuga, eos at. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti cum accusantium magni expedita in ratione, ipsam rerum porro delectus quo. Eaque, voluptates repellat reiciendis ut ipsam odit molestiae expedita eius!',
 			parts:[
-				{id:203, name:'Cras justo odio',qty:14},
-				{id:203, name:'Rusto odio',qty:1},
-				{id:203, name:'Dio',qty:3}
-			]
-		},
-		{
-			id:1121,
-			company:'Company Name541',
-			type:'Lorem ipsum dolor sit amet',
-			startTime:'3:00 PM',
-			contact:{
-				name:'Steve Doe',
-				address:'1355 Market Street, Suit 900',
-				city:'Some City',
-				state:'NJ',
-				zip:'08000',
-				phone:'(123) 456-7890'
-			},
-			description:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam esse voluptatum, reiciendis eius pariatur blanditiis vel ducimus doloribus. Sed id, fuga dolorem. Assumenda adipisci laboriosam rem deserunt fuga, eos at. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti cum accusantium magni expedita in ratione, ipsam rerum porro delectus quo. Eaque, voluptates repellat reiciendis ut ipsam odit molestiae expedita eius!',
-			instrucitons:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam esse voluptatum, reiciendis eius pariatur blanditiis vel ducimus doloribus. Sed id, fuga dolorem. Assumenda adipisci laboriosam rem deserunt fuga, eos at. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti cum accusantium magni expedita in ratione, ipsam rerum porro delectus quo. Eaque, voluptates repellat reiciendis ut ipsam odit molestiae expedita eius!',
-			parts:[
-				{id:203, name:'Cras justo odio',qty:14},
-				{id:203, name:'Rusto odio',qty:1},
-				{id:203, name:'Dio',qty:3}
+				{id:203, name:'Washer 3/4',qty:9},
+				{id:203, name:'Nut HEX 3/4-10',qty:11},
+				{id:203, name:'CLPC HSE',qty:4}
 			]
 		}
 	];
